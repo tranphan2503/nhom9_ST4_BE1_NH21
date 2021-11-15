b585f9099855c78c32e17b423f60e249cf06e2b7
Hiếu <0lehieu0@gmail.com>
2021-11-15 17:04:48 +0700

    Update ggmap

--- header.php
+++ header.php
@@ -59,7 +59,7 @@ $getManuName = $product->getManuName();
 					<ul class="header-links pull-left">
 						<li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
 						<li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
-						<li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+						<li><a href="maps.html"><i class="fa fa-map-marker"></i>720A Điện Biên Phủ, Phường 22, Bình Thạnh, Thành phố Hồ Chí Minh</a></li>
 					</ul>
 					<ul class="header-links pull-right">
 						<li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
